e89061ae9a363c89612c4df4772d2355d99fb3b2
Ashritha <ashritha1255@gmail.com>
2026-07-14 00:28:19 +0530

    Allow PATCH requests for task completion

--- src/main/java/taskmanagementsystem/security/WebSecurityConfig.java
+++ src/main/java/taskmanagementsystem/security/WebSecurityConfig.java
@@ -69,6 +69,7 @@ public class WebSecurityConfig {
                 "GET",
                 "POST",
                 "PUT",
+                "PATCH",
                 "DELETE",
                 "OPTIONS"
         ));
