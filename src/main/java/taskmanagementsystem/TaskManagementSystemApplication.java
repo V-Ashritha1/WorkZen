e542c7ecb97b563481009244ac3627ca8d360f13
Ashritha <ashritha1255@gmail.com>
2026-07-13 23:39:10 +0530

    Fix CORS configuration

--- src/main/java/taskmanagementsystem/TaskManagementSystemApplication.java
+++ src/main/java/taskmanagementsystem/TaskManagementSystemApplication.java
@@ -3,7 +3,7 @@ package taskmanagementsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@CrossOrigin(origins = "https://work-8ecn1vgo0-ashritha12.vercel.app")
+
 @SpringBootApplication
 public class TaskManagementSystemApplication {
 
